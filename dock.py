c4fa9bcb10ad43f7fa8ff60c7d667d0188a61cbc
carolinabarandica <carolinabarandica1@gmail.com>
2025-03-19 18:11:21 +0100

    new commit

---
+++ dock.py
@@ -0,0 +1,17 @@
+class Card:
+    ranks = ['2','3','4','5','6','7','8','9','10',
+             'J', 'Q', 'K','A']
+    suits = ['♠', '♥', '♦', '♣']
+    def __init__(self, suit, rank):
+        if rank not in self.ranks:
+            raise ValueError('invalid rank')
+        if suit not in self.suits:
+            raise ValueError('invalid suit')
+        self._suit = suit
+        self._rank = rank
+
+    def __str__(self):
+        return f"{self._rank}{self._suit}"
+
+jack_clubs= Card('♠', 'J')
+print(jack_clubs)
